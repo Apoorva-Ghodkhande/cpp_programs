e5404757825a70a36ef2a892e4d731edd26a4553
Apoorva-Ghodkhande <89741261+Apoorva-Ghodkhande@users.noreply.github.com>
2023-06-04 14:03:44 +0530

    Linked list 2

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -32,7 +32,7 @@ length=1;
 Node* temp=head;
 while(temp)
 {
-std::cout << ' '<<temp->value<<std::endl;
+std::cout <<temp->value<<std::endl;
 temp=temp->next;
 }
 }
@@ -52,9 +52,79 @@ void getLength()
     std::cout<<"Length:"<< length <<std::endl;
 }
 
-void appendList()
+void appendList(int value)
 {
-    
+       Node* newNode=new Node(value);
+    if(length==0)
+    {
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+     
+        tail->next=newNode;
+        tail=newNode;
+
+    }
+    tail->next=nullptr;
+    length++;
+}
+
+void deleteLast()
+{
+
+    if(length==0) return;
+    Node* temp=head;
+     if(length==1)
+    {
+        head=nullptr;
+        tail=nullptr;
+    }
+    else{
+        Node* pre=head;
+         while(temp->next){
+            pre=temp;
+            temp=temp->next;
+        }
+tail=pre;
+tail->next=nullptr;
+    }
+    delete temp;
+    length--;
+}
+
+void prepend(int value)
+{
+    Node* newNode= new Node(value);
+
+    if(length==0)
+    {
+     head=newNode;
+     tail=newNode;
+     tail->next=nullptr;
+    }
+    else{
+        newNode->next=head;
+        head=newNode;
+    }
+    length++;
+
+}
+
+void deleteFirst()
+{
+    if(length==0)return;
+    Node* temp=head;
+    if(length==1)
+    {
+     head=nullptr;
+     tail=nullptr;
+    }else
+    {
+    head=head->next;
+    }
+    length--;
+    delete temp;
 }
 
 ~LinkedList()
@@ -66,9 +136,6 @@ void appendList()
      delete temp;
      temp=head;
     }
-    {
-        /* code */
-    }
     
 }
 };
@@ -79,7 +146,12 @@ int main()
  list->getHead();
  list->getTail();
  list->getLength();
- list->printList();
-
-    
+ //list->printList();
+list->appendList(9);
+list->appendList(5);
+//list->printList();
+  list->deleteLast();
+  list->prepend(1);
+  list->deleteFirst();
+  list->printList(); 
 }
